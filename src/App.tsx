b15cb06a0da8475b801676ce130f218078f54377
jessi68 <lyn990312@gmail.com>
2022-03-27 11:43:02 +0900

    configure: configure webpack, babel, tsconfig for frontend

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function a(b: Number) {
+    return b;
+}
+
+function App() {
+    return (
+        <div>
+            코드포코리아 기후변화
+        </div>
+    )
+}
+
+export default App
