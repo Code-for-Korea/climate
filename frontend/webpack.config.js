e3b3569ec88fdf38e1025265b80c089a2a42bfd7
jessi68 <lyn990312@gmail.com>
2022-03-29 21:52:09 +0900

    config: install tailwind and configured

---
+++ frontend/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  mode,
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true
+  },
+  entry: {
+    app: path.join(__dirname,  'index.tsx'),
+  },
+ 
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ["css-loader", "postcss-loader"],
+      },
+    ],
+  },
+
+	output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+		new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+};
