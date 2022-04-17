6e33bad15fc0a6df2c0df77a4e57dc1b42d9a5b8
Ohyeon <rest515@gmail.com>
2022-04-18 08:57:04 +0900

    add administrate

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       root to: "tools#index"
     end
 
-    resources :tools
+  resources :tools
   resources :data_sources
   resources :goals
   resources :documents
