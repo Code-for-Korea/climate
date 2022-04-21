b3b46eff434ceeee10334ff481dae57135ffec61
jessi68 <lyn990312@gmail.com>
2022-04-22 01:41:27 +0900

    fix: fix font size and weight of title in detailgridbox

--- frontend/src/init/App.tsx
+++ frontend/src/init/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
     return <div className="flex flex-col">
         <div className="m-auto">
-            <h1 className="mb-8 mt-3">2030 NDC 부문별 목표와 현황</h1>
+            <div className="mb-8 mt-3 font-bold text-xl">2030 NDC 부문별 목표와 현황</div>
         <DetailAreaGrid detailArea="건물" carbonDetailDatas={metaData}/>   
         </div>
            
